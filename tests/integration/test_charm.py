0a505706ed9e71378d2d2f6e3f39b50f428b4987
Adam Dyess <adam.dyess@canonical.com>
2023-12-04 15:53:12 -0600

    Simplify charm and tests to easily pass

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -27,12 +27,9 @@ async def test_build_and_deploy(ops_test: OpsTest, pytestconfig: pytest.Config):
     """
     # Deploy the charm and wait for active/idle status
     charm = pytestconfig.getoption("--charm-file")
-    resources = {"httpbin-image": METADATA["resources"]["httpbin-image"]["upstream-source"]}
     assert ops_test.model
     await asyncio.gather(
-        ops_test.model.deploy(
-            f"./{charm}", resources=resources, application_name=APP_NAME, series="jammy"
-        ),
+        ops_test.model.deploy(f"./{charm}", application_name=APP_NAME, series="jammy"),
         ops_test.model.wait_for_idle(
             apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
         ),
