097e356212666a49f3666f485b5c97963cd7ddc0
Mateo Florido <mateo.florido@canonical.com>
2024-01-05 11:02:22 -0500

    Ignore Test in static check

--- charms/k8s/tests/unit/test_k8sd_api_manager.py
+++ charms/k8s/tests/unit/test_k8sd_api_manager.py
@@ -104,6 +104,7 @@ class TestK8sdAPIManager(unittest.TestCase):
     """Test K8sdAPIManager"""
 
     def setUp(self):
+        """Setup environment."""
         self.mock_factory = MagicMock()
         self.api_manager = K8sdAPIManager(factory=self.mock_factory)
 
