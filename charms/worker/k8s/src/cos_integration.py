69c887b1b7f1c969a42588408ceeface021a49ba
Mateo Florido <32885896+mateoflorido@users.noreply.github.com>
2024-02-20 16:09:53 -0500

    Implement `cos-agent` integration (#24)
    * Implement COS agent integration
    
    * Refactor COSIntegration
    
    * Add License headers

---
+++ charms/worker/k8s/src/cos_integration.py
@@ -0,0 +1,186 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""COS Integration module."""
+
+import logging
+from dataclasses import dataclass
+from typing import Dict, List
+
+import ops
+from ops.charm import CharmBase
+
+log = logging.getLogger(__name__)
+
+OBSERVABILITY_ROLE = "system:cos"
+
+
+@dataclass
+class JobConfig:
+    """Data class representing the configuration for a Prometheus scrape job.
+
+    Attributes:
+        name (str): The name of the scrape job. Corresponds to the name of the Kubernetes
+                    component being monitored (e.g., 'kube-apiserver').
+        metrics_path (str): The endpoint path where the metrics are exposed by the
+                            component (e.g., '/metrics').
+        scheme (str): The scheme used for the endpoint. (e.g.'http' or 'https').
+        target (str): The network address of the target component along with
+                      the port.
+                      Format is 'hostname:port' (e.g., 'localhost:6443').
+        relabel_configs (List[Dict[str, str | List[str]]]): Additional
+                      configurations for relabeling.
+    """
+
+    name: str
+    metrics_path: str
+    scheme: str
+    target: str
+    relabel_configs: List[Dict[str, str | List[str]]]
+
+
+class COSIntegration(ops.Object):
+    """Utility class that handles the integration with COS.
+
+    This class provides methods to retrieve and configure Prometheus metrics
+    scraping endpoints based on the Kubernetes components running within
+    the cluster.
+
+    Attributes:
+        charm (CharmBase): Reference to the base charm instance.
+    """
+
+    def __init__(self, charm: CharmBase) -> None:
+        """Initialize a COSIntegration instance.
+
+        Args:
+            charm (CharmBase): A charm object representing the current charm.
+        """
+        super().__init__(charm, "cos-integration")
+        self.charm = charm
+
+    def _create_scrape_job(self, config: JobConfig, node_name: str, token: str) -> dict:
+        """Create a scrape job configuration.
+
+        Args:
+            config (JobConfig): The configuration for the scrape job.
+            node_name (str): The name of the node.
+            token (str): The token for authorization.
+
+        Returns:
+            dict: The scrape job configuration.
+        """
+        return {
+            "tls_config": {"insecure_skip_verify": True},
+            "authorization": {"credentials": token},
+            "job_name": config.name,
+            "metrics_path": config.metrics_path,
+            "scheme": config.scheme,
+            "static_configs": [
+                {
+                    "targets": [config.target],
+                    "labels": {"node": node_name, "cluster": self.charm.model.name},
+                }
+            ],
+            "relabel_configs": config.relabel_configs,
+        }
+
+    def get_metrics_endpoints(
+        self, node_name: str, token: str, control_plane: bool = False
+    ) -> List[Dict]:
+        """Retrieve Prometheus scrape job configurations for Kubernetes components.
+
+        Args:
+            node_name (str): The name of the node.
+            token (str): The authentication token.
+            control_plane (bool, optional): If True, include control plane components.
+                Defaults to False.
+
+        Returns:
+            List[Dict]: A list of Prometheus scrape job configurations.
+        """
+        log.info("Building Prometheus scraping jobs.")
+
+        control_plane_jobs = [
+            JobConfig(
+                "apiserver",
+                "/metrics",
+                "https",
+                "localhost:6443",
+                [
+                    {
+                        "source_labels": ["job"],
+                        "target_label": "job",
+                        "replacement": "apiserver",
+                    }
+                ],
+            ),
+            JobConfig(
+                "kube-scheduler",
+                "/metrics",
+                "https",
+                "localhost:10259",
+                [{"target_label": "job", "replacement": "kube-scheduler"}],
+            ),
+            JobConfig(
+                "kube-controller-manager",
+                "/metrics",
+                "https",
+                "localhost:10257",
+                [{"target_label": "job", "replacement": "kube-controller-manager"}],
+            ),
+        ]
+
+        shared_jobs = [
+            JobConfig(
+                "kube-proxy",
+                "/metrics",
+                "http",
+                "localhost:10249",
+                [{"target_label": "job", "replacement": "kube-proxy"}],
+            ),
+        ]
+
+        kubelet_metrics_paths = [
+            "/metrics",
+            "/metrics/resource",
+            "/metrics/cadvisor",
+            "/metrics/probes",
+        ]
+
+        kubelet_jobs = [
+            JobConfig(
+                f"kubelet-{metric}" if metric else "kubelet",
+                path,
+                "https",
+                "localhost:10250",
+                [
+                    {"target_label": "metrics_path", "replacement": path},
+                    {"target_label": "job", "replacement": "kubelet"},
+                ],
+            )
+            for path in kubelet_metrics_paths
+            if (metric := path.strip("/metrics")) is not None
+        ]
+        kube_state_metrics = (
+            [
+                JobConfig(
+                    "kube-state-metrics",
+                    "/api/v1/namespaces/kube-system/services/"
+                    + "kube-state-metrics:8080/proxy/metrics",
+                    "https",
+                    "localhost:6443",
+                    [
+                        {"target_label": "job", "replacement": "kube-state-metrics"},
+                    ],
+                )
+            ]
+            if self.charm.unit.is_leader()
+            else []
+        )
+
+        jobs = shared_jobs + kubelet_jobs
+        if control_plane:
+            jobs += control_plane_jobs + kube_state_metrics
+
+        return [self._create_scrape_job(job, node_name, token) for job in jobs]
---
+++ charms/worker/k8s/src/token_distributor.py
@@ -0,0 +1,129 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Token Distributor module."""
+
+from enum import Enum, auto
+
+import ops
+from charms.k8s.v0.k8sd_api_manager import K8sdAPIManager
+from ops import CharmBase
+
+
+class TokenStrategy(Enum):
+    """Enumeration defining strategy for token creation.
+
+    Attributes:
+        CLUSTER: Strategy for creating cluster tokens.
+        COS: Strategy for creating COS tokens.
+    """
+
+    CLUSTER = auto()
+    COS = auto()
+
+
+class ClusterTokenType(Enum):
+    """Enumeration defining types for cluster tokens.
+
+    Attributes:
+        CONTROL_PLANE: Token type for control plane.
+        WORKER: Token type for worker nodes.
+        NONE: No specific token type.
+    """
+
+    CONTROL_PLANE = "control-plane"
+    WORKER = "worker"
+    NONE = ""
+
+
+class TokenDistributor:
+    """Helper class for distributing tokens to units in a relation."""
+
+    def __init__(self, api_manager: K8sdAPIManager, charm: CharmBase):
+        """Initialize a TokenDistributor instance.
+
+        Args:
+            api_manager: An K8sdAPIManager object for interacting with k8sd API.
+            charm (CharmBase): A charm object representing the current charm.
+        """
+        self.api_manager = api_manager
+        self.charm = charm
+        self.token_creation_strategies = {
+            TokenStrategy.CLUSTER: self._create_cluster_token,
+            TokenStrategy.COS: self._create_cos_token,
+        }
+
+    def _create_cluster_token(self, name: str, token_type: ClusterTokenType):
+        """Create a cluster token.
+
+        Args:
+            name (str): The name of the node.
+            token_type (ClusterTokenType): The type of cluster token.
+
+        Returns:
+            str: The created cluster token.
+        """
+        worker = token_type == ClusterTokenType.WORKER
+        return self.api_manager.create_join_token(name, worker=worker)
+
+    def _create_cos_token(self, name: str, _):
+        """Create a COS token.
+
+        Args:
+            name (str): The name of the node.
+
+        Returns:
+            str: The created COS token.
+        """
+        return self.api_manager.request_auth_token(
+            username=f"system:cos:{name}", groups=["system:cos"]
+        )
+
+    def distribute_tokens(
+        self,
+        relation: ops.Relation,
+        token_strategy: TokenStrategy,
+        token_type: ClusterTokenType = ClusterTokenType.NONE,
+        all_units: bool = False,
+    ):
+        """Distribute tokens to units in a relation.
+
+        Args:
+            relation (ops.Relation): The relation object.
+            token_strategy (TokenStrategy): The strategy of token creation.
+            token_type (ClusterTokenType, optional): The type of cluster token.
+                Defaults to ClusterTokenType.NONE.
+            all_units (bool, optional): Whether to include all units in the relation.
+                Defaults to False.
+
+        Raises:
+            ValueError: If an invalid token_strategy is provided.
+        """
+        units = relation.units
+        if all_units:
+            units.add(self.charm.unit)
+
+        app_databag: ops.RelationDataContent | dict[str, str] = relation.data.get(
+            self.charm.model.app, {}
+        )
+        sec_key_suffix = (
+            "-cluster-secret" if token_strategy == TokenStrategy.CLUSTER else "-cos-secret"
+        )
+
+        for unit in units:
+            sec_key = f"{unit.name}{sec_key_suffix}"
+            if app_databag.get(sec_key):
+                continue
+            if not (name := relation.data[unit].get("node-name")):
+                continue  # wait for the joining unit to provide its node-name
+
+            # Select the appropriate token creation strategy
+            token_creator = self.token_creation_strategies.get(token_strategy)
+            if not token_creator:
+                raise ValueError(f"Invalid token_strategy: {token_strategy}")
+
+            token = token_creator(name, token_type)
+            content = {"token": token}
+            secret = self.charm.app.add_secret(content)
+            secret.grant(relation, unit=unit)
+            relation.data[self.charm.app][sec_key] = secret.id or ""
