03d491bc9f58764bea0fe82c67deb3bf085a320e
Adam Dyess <adam.dyess@canonical.com>
2024-02-22 16:08:43 -0600

    Add node-labeling charm config using the LabelMaker library (#23)
    * Add node-labeling charm config using the LabelMaker library
    
    * Improving unit tests
    
    * Apply node labels using node kubeconfig
    
    * LabelMaker uses source kubeconfig
    
    * Pass lint after merge from main
    
    * Don't label workers by default with node-role due to lack of permissions
    
    * linting issues
    
    * simplify and retry failed snap installs

--- tests/integration/test_k8s.py
+++ tests/integration/test_k8s.py
@@ -14,6 +14,24 @@ from tenacity import retry, stop_after_attempt, wait_fixed
 log = logging.getLogger(__name__)
 
 
+async def get_nodes(k8s):
+    """Return Node list
+
+    Args:
+        k8s: any k8s unit
+
+    Returns:
+        list of nodes
+    """
+    action = await k8s.run("k8s kubectl get nodes -o json")
+    result = await action.wait()
+    assert result.results["return-code"] == 0, "Failed to get nodes with kubectl"
+    log.info("Parsing node list...")
+    node_list = json.loads(result.results["stdout"])
+    assert node_list["kind"] == "List", "Should have found a list of nodes"
+    return node_list["items"]
+
+
 @retry(reraise=True, stop=stop_after_attempt(12), wait=wait_fixed(15))
 async def ready_nodes(k8s, expected_count):
     """Get a list of the ready nodes.
@@ -21,31 +39,22 @@ async def ready_nodes(k8s, expected_count):
     Args:
         k8s: k8s unit
         expected_count: number of expected nodes
-
-    Returns:
-        list of nodes
     """
     log.info("Finding all nodes...")
-    action = await k8s.run("k8s kubectl get nodes -o json")
-    result = await action.wait()
-    assert result.results["return-code"] == 0, "Failed to get nodes with kubectl"
-    log.info("Parsing node list...")
-    node_list = json.loads(result.results["stdout"])
-    assert node_list["kind"] == "List", "Should have found a list of nodes"
-    nodes = {
+    nodes = await get_nodes(k8s)
+    ready_nodes = {
         node["metadata"]["name"]: all(
             condition["status"] == "False"
             for condition in node["status"]["conditions"]
             if condition["type"] != "Ready"
         )
-        for node in node_list["items"]
+        for node in nodes
     }
-    log.info("Found %d/%d nodes...", len(nodes), expected_count)
-    assert len(nodes) == expected_count, f"Expect {expected_count} nodes in the list"
-    for node, ready in nodes.items():
+    log.info("Found %d/%d nodes...", len(ready_nodes), expected_count)
+    assert len(ready_nodes) == expected_count, f"Expect {expected_count} nodes in the list"
+    for node, ready in ready_nodes.items():
         log.info("Node %s is %s..", node, "ready" if ready else "not ready")
         assert ready, f"Node not yet ready: {node}."
-    return nodes
 
 
 @pytest.mark.abort_on_fail
@@ -55,3 +64,15 @@ async def test_nodes_ready(kubernetes_cluster):
     worker = kubernetes_cluster.applications["k8s-worker"]
     expected_nodes = len(k8s.units) + len(worker.units)
     await ready_nodes(k8s.units[0], expected_nodes)
+
+
+async def test_nodes_labelled(kubernetes_cluster):
+    """Test the charms label the nodes appropriately."""
+    k8s = kubernetes_cluster.applications["k8s"]
+    worker = kubernetes_cluster.applications["k8s-worker"]
+    nodes = await get_nodes(k8s.units[0])
+    control_plane_label = "node-role.kubernetes.io/control-plane"
+    control_plane = [n for n in nodes if control_plane_label in n["metadata"]["labels"]]
+    assert len(k8s.units) == len(control_plane), "Not all control-plane nodes labeled"
+    juju_nodes = [n for n in nodes if "juju-charm" in n["metadata"]["labels"]]
+    assert len(k8s.units + worker.units) == len(juju_nodes), "Not all nodes labeled as juju-charms"
