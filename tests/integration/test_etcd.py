c752b27a3512d7dc0da606db61f08f2021233dc4
Mateo Florido <mateo.florido@canonical.com>
2024-03-22 17:57:58 -0500

    Fix nodes count

--- tests/integration/test_etcd.py
+++ tests/integration/test_etcd.py
@@ -67,5 +67,6 @@ async def ready_nodes(k8s, expected_count):
 async def test_nodes_ready(kubernetes_cluster: model.Model):
     """Deploy the charm and wait for active/idle status."""
     k8s = kubernetes_cluster.applications["k8s"]
-    expected_nodes = len(k8s.units)
+    worker = kubernetes_cluster.applications["k8s-worker"]
+    expected_nodes = len(k8s.units) + len(worker.units)
     await ready_nodes(k8s.units[0], expected_nodes)
