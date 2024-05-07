441d831465ab2936655078ae2fe647fdaad1e81c
Adam Dyess <adam.dyess@canonical.com>
2024-05-07 16:55:41 -0500

    [Release 1.30] Snap Revision Update (#104)
    * Pin arm64 snap revision to 314
    * Hold snap revision forever
    * Update revision follows the 1.30-classic/stable channel
    * rename update-snap-revision method

--- .github/workflows/update-snap-revision.py
+++ .github/workflows/update-snap-revision.py
@@ -11,12 +11,19 @@ from pathlib import Path
 from urllib.request import Request, urlopen
 import yaml
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format="%(levelname)-8s: %(message)s", level=logging.INFO)
 log = logging.getLogger("update-snap-revision")
-
+TRACK = "1.30-classic"
+RISK = "stable"
 ROOT = Path(__file__).parent / ".." / ".."
 INSTALLATION = ROOT / "charms/worker/k8s/templates/snap_installation.yaml"
 LICENSE = Path(__file__).read_text().splitlines(keepends=True)[1:4]
+
+
+def _multiline_log(logger, message, *args, **kwargs):
+    NEWLINE_INDENT = "\n          * "
+    logger(message.replace("\n", NEWLINE_INDENT), *args, **kwargs)
+
 
 def find_current_revision(arch: str) -> None | str:
     content = yaml.safe_load(INSTALLATION.read_text())
@@ -24,11 +31,11 @@ def find_current_revision(arch: str) -> None | str:
         for value in arch_spec:
             if value.get("name") == "k8s":
                 rev = value.get("revision")
-                log.info("Currently arch=%s revision=%s", arch, rev)
+                log.info("Currently arch='%s' revision='%s'", arch, rev)
                 return rev
 
 
-def find_snapstore_revision(track: str, arch: str, risk: str) -> str:
+def find_snapstore_revision(arch: str, track: str, risk: str) -> str:
     URL = f"https://api.snapcraft.io/v2/snaps/info/k8s?architecture={arch}&fields=revision"
     HEADER = {"Snap-Device-Series": 16}
     req = Request(URL, headers=HEADER)
@@ -42,9 +49,22 @@ def find_snapstore_revision(track: str, arch: str, risk: str) -> str:
             and track in channel.get("track")
         ):
             rev = mapping.get("revision")
-            log.info("SnapStore arch=%s revision=%s track=%s%s", arch, rev, track, f" risk={risk}" if risk else "")
+            log.info(
+                "SnapStore arch='%s' revision='%s' track='%s'%s",
+                arch,
+                rev,
+                track,
+                f" risk='{risk}'" if risk else "",
+            )
             return rev
-    log.warning("SnapStore arch=%s revision=%s track=%s%s", arch, "N/A", track, f" risk={risk}" if risk else "")
+    _multiline_log(
+        log.warning,
+        "Failed to find a revision matching the arch/track/risk\n"
+        "SnapStore arch='%s' track='%s'%s",
+        arch,
+        track,
+        f" risk='{risk}'" if risk else "",
+    )
 
 
 def update_current_revision(arch: str, rev: str):
@@ -59,22 +79,18 @@ def update_current_revision(arch: str, rev: str):
         f.write(yaml.safe_dump(content))
 
 
-def update_github_env(variable:str, value:str):
+def update_github_output(variable: str, value: str):
     if github_output := os.environ.get("GITHUB_OUTPUT", None):
         with Path(github_output).open(mode="a+") as f:
             f.write(f"{variable}={value}")
 
 
 if __name__ == "__main__":
-    arch, track, risk = sys.argv[1:]
+    arch, *_ = sys.argv[1:]
     current_rev = find_current_revision(arch)
-    snapstore_rev = find_snapstore_revision(track, arch, risk)
-    if (
-        snapstore_rev and
-        current_rev and
-        current_rev != snapstore_rev
-    ):
+    snapstore_rev = find_snapstore_revision(arch, TRACK, RISK)
+    if snapstore_rev and current_rev and current_rev != snapstore_rev:
         update_current_revision(arch, snapstore_rev)
-        update_github_env("result", snapstore_rev)
+        update_github_output("result", snapstore_rev)
     else:
-        log.info("No change arch=%s current=%s snapstore=%s", arch, current_rev, snapstore_rev)        
+        log.info("No change arch=%s current=%s snapstore=%s", arch, current_rev, snapstore_rev)
