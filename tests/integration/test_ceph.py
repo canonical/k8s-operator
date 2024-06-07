5500193915aa7249995b1b5ff93be6a7a4e2fd6d
Adam Dyess <adam.dyess@canonical.com>
2024-06-06 20:24:35 -0500

    Apply review comments

--- tests/integration/test_ceph.py
+++ tests/integration/test_ceph.py
@@ -9,23 +9,12 @@
 import pytest
 from juju import model, unit
 
-from .helpers import ready_nodes
-
 # This pytest mark configures the test environment to use the Canonical Kubernetes
 # bundle with ceph, for all the test within this module.
 pytestmark = [
     pytest.mark.bundle_file("test-bundle-ceph.yaml"),
     pytest.mark.ignore_blocked,
 ]
-
-
-@pytest.mark.abort_on_fail
-async def test_nodes_ready(kubernetes_cluster: model.Model):
-    """Deploy the charm and wait for active/idle status."""
-    k8s = kubernetes_cluster.applications["k8s"]
-    worker = kubernetes_cluster.applications["k8s-worker"]
-    expected_nodes = len(k8s.units) + len(worker.units)
-    await ready_nodes(k8s.units[0], expected_nodes)
 
 
 @pytest.mark.abort_on_fail
