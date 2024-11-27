c9a83693a31365618c99615860b997e488e582ef
Alex Balderson <alexander.balderson@canonical.com>
2024-11-27 13:32:29 -0700

    inital pass at adding terraform for the k8s charms

---
+++ charms/worker/k8s/terraform/main.tf
@@ -0,0 +1,19 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+resource "juju_application" "k8s" {
+  name  = var.app_name
+  model = var.model
+
+  charm {
+    name     = "k8s"
+    channel  = var.channel
+    revision = var.revision
+    series   = var.series
+  }
+
+  config      = var.config
+  constraints = var.constraints
+  units       = var.units
+  resources   = var.resources
+}
---
+++ charms/worker/k8s/terraform/variables.tf
@@ -0,0 +1,61 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+variable "app_name" {
+  description = "Name of the application in the Juju model."
+  type        = string
+  default     = "k8s"
+}
+
+variable "channel" {
+  description = "The channel to use when deploying a charm."
+  type        = string
+  default     = "1.30/edge"
+}
+
+variable "config" {
+  description = "Application config. Details about available options can be found at https://charmhub.io/k8s/configurations."
+  type        = map(string)
+  default     = {}
+}
+
+variable "constraints" {
+  description = "Juju constraints to apply for this application."
+  type        = string
+  default     = "arch=amd64"
+}
+
+variable "model" {
+  description = "Reference to a `juju_model`."
+  type        = string
+  default     = ""
+}
+
+variable "resources" {
+  description = "Resources to use with the application. Details about available options can be found at https://charmhub.io/k8s/configurations."
+  type        = map(string)
+  default     = {}
+}
+
+variable "revision" {
+  description = "Revision number of the charm"
+  type        = number
+  default     = null
+}
+
+variable "series" {
+  description = "Ubuntu series to deploy the charm onto"
+  type        = string
+  default     = "24.04"
+
+  validation {
+    condition     = contains(["20.04", "22.04", "24.04"], var.series)
+    error_message = "Series must be one of 20.04, 22.04, 24.04"
+  }
+}
+
+variable "units" {
+  description = "Number of units to deploy"
+  type        = number
+  default     = 1
+}
