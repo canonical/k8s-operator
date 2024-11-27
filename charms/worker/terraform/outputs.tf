9797485d1b7f67ed63954b2539b7da2dbcb95746
Alex Balderson <alexander.balderson@canonical.com>
2024-11-27 13:32:29 -0700

    inital pass at adding terraform for the k8s charms

---
+++ charms/worker/terraform/outputs.tf
@@ -0,0 +1,24 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+output "app_name" {
+  description = "Name of the deployed application."
+  value       = juju_application.k8s_worker.name
+}
+
+output "requires" {
+  value = {
+    aws         = "aws-integration"
+    azure       = "azure-integration"
+    cluster     = "k8s-cluster"
+    cos_tokens  = "cos-k8s-tokens"
+    containerd  = "containerd"
+    gcp         = "gcp-integration"
+  }
+}
+
+output "provides" {
+  value = {
+    cos_agent   = "cos_agent"
+  }
+}
