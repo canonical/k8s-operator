ea19fdb863c655c0bd24f8f5461ef7fc18882779
Adam Dyess <adam.dyess@canonical.com>
2025-01-22 10:47:47 -0600

    Rather than /etc/environment, map to containerd system file

--- charms/worker/k8s/src/literals.py
+++ charms/worker/k8s/src/literals.py
@@ -13,6 +13,7 @@ VALID_LOG_LEVELS = ["info", "debug", "warning", "error", "critical"]
 
 # Charm
 CONTAINERD_BASE_PATH = Path("/etc/containerd")
+CONTAINERD_HTTP_PROXY = Path("/etc/systemd/system/snap.k8s.containerd.service.d/http-proxy.conf")
 ETC_KUBERNETES = Path("/etc/kubernetes")
 HOSTSD_PATH = CONTAINERD_BASE_PATH / "hosts.d/"
 KUBECONFIG = Path.home() / ".kube/config"
