223c7d4b758809424ddc73c15c756994ebccd0bd
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-01-28 15:18:41 +0100

    fix lint and licencing

--- tests/integration/terraform_setup.py
+++ tests/integration/terraform_setup.py
@@ -3,40 +3,42 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 """
-This script deploys a Kubernetes cluster using the Juju Terraform provider and a
-manifest as input. See https://github.com/asbalderson/k8s-bundles/blob/terraform-bundle-basic/terraform/README.md
+This script deploys a Kubernetes cluster using the Juju
+Terraform provider and a manifest as input.
+See https://github.com/asbalderson/k8s-bundles/blob/terraform-bundle-basic/terraform/README.md
 
-Key Features:
-- Verifies if Terraform is installed and at the specified version. If not,
-  installs the correct version using the Snap package manager.
-- Sets up Juju provider authentication by exporting necessary credentials
-  and connection details from the Juju configuration.
-- Checks if a specified Juju model exists; if not, creates the model.
-- Applies a specified LXD profile (`k8s.profile`) to the Juju model if the
-  controller's cloud is LXD or localhost.
-- Initializes Terraform, validates the plan, and applies it with the
-  provided manifest and model name.
-
-Inputs (Command-Line Arguments):
+Command-Line Arguments:
 - `--version`: Specifies the expected version of Terraform to be installed
   (default: latest/stable).
-- `--path`: The path to the Terraform module directory (default: the script's directory).
-- `--manifest`: Path to the YAML manifest file used by Terraform (default: `default-manifest.yaml` in the script's directory).
-- `--model-name`: The name of the Juju model to operate on (default: `my-canonical-k8s`).
+- `--path`: The path to the Terraform module directory
+  (default: the script's directory).
+- `--manifest`: Path to the YAML manifest file used by Terraform
+  (default: `default-manifest.yaml` in the script's directory).
+- `--model-name`: The name of the Juju model to operate on
+  (default: `my-canonical-k8s`).
 
 Usage:
-    python3 script.py --version latest/stable --path /path/to/module --manifest /path/to/manifest.yaml --model-name custom-model
+    python3 script.py
 """
 
 import argparse
-from pathlib import Path
 import os
 import subprocess
 import sys
+from pathlib import Path
 
 
-def run_command(command, capture_output=False, fail_on_error=True):
-    """Run a shell command."""
+def run_command(command, capture_output=False, fail_on_error=True) -> str | None:
+    """Run a shell command.
+
+    Args:
+        command: The shell command to run.
+        capture_output: Whether to capture the command's output.
+        fail_on_error: Whether to exit the script if the command fails.
+
+    Returns:
+        The command's output if `capture_output` is `True`, otherwise `None`.
+    """
     try:
         result = subprocess.run(
             command, shell=True, check=True, text=True, capture_output=capture_output
@@ -46,10 +48,15 @@ def run_command(command, capture_output=False, fail_on_error=True):
         if fail_on_error:
             print(f"Error running command: {command}\n{e}")
             sys.exit(1)
+        return None
 
 
-def ensure_terraform(expected_version):
-    """Ensure Terraform is installed and matches the expected version."""
+def ensure_terraform(expected_version) -> None:
+    """Ensure Terraform is installed and matches the expected version.
+
+    Args:
+        expected_version: The expected version of Terraform.
+    """
     installed_version = run_command(
         "snap list terraform | awk '/^terraform/ {print $4}'", capture_output=True
     )
@@ -58,7 +65,8 @@ def ensure_terraform(expected_version):
         run_command(f"sudo snap install terraform --channel={expected_version} --classic")
     elif installed_version != expected_version:
         print(
-            f"Error: Installed Terraform version ({installed_version}) does not match the expected version ({expected_version})."
+            f"Error: Installed Terraform version ({installed_version})"
+            f"does not match the expected version ({expected_version})."
         )
         sys.exit(1)
     else:
@@ -76,28 +84,32 @@ def setup_juju_provider_authentication():
     os.environ["CONTROLLER"] = controller or ""
     os.environ["JUJU_CONTROLLER_ADDRESSES"] = (
         run_command(
-            f"juju show-controller | yq .{controller}.details.api-endpoints | yq -r '. | join(\",\")'",
+            f"juju show-controller | yq .{controller}.details.api-endpoints"
+            "| yq -r '. | join(\",\")'",
             capture_output=True,
         )
         or ""
     )
     os.environ["JUJU_USERNAME"] = (
         run_command(
-            f"cat ~/.local/share/juju/accounts.yaml | yq .controllers.{controller}.user | tr -d '\"'",
+            f"cat ~/.local/share/juju/accounts.yaml"
+            f"| yq .controllers.{controller}.user | tr -d '\"'",
             capture_output=True,
         )
         or ""
     )
     os.environ["JUJU_PASSWORD"] = (
         run_command(
-            f"cat ~/.local/share/juju/accounts.yaml | yq .controllers.{controller}.password | tr -d '\"'",
+            f"cat ~/.local/share/juju/accounts.yaml"
+            f"| yq .controllers.{controller}.password | tr -d '\"'",
             capture_output=True,
         )
         or ""
     )
     os.environ["JUJU_CA_CERT"] = (
         run_command(
-            f"juju show-controller {controller.strip()} | yq '.{controller}.details.\"ca-cert\"' | tr -d '\"' | sed 's/\\\\n/\\n/g'",
+            f"juju show-controller {controller.strip()}"
+            f"| yq '.{controller}.details.\"ca-cert\"' | tr -d '\"' | sed 's/\\\\n/\\n/g'",
             capture_output=True,
         )
         or ""
@@ -105,9 +117,18 @@ def setup_juju_provider_authentication():
 
 
 def ensure_model_exists(model_name, lxd_profile_path):
-    """Ensure the specified Juju model exists. If not, create it and configure required LXD profile."""
+    """Ensure the specified Juju model exists.
+
+    If not, create it and configure LXD profile if required.
+
+    Args:
+        model_name: The name of the Juju model.
+        lxd_profile_path: The path to the LXD profile file.
+    """
     # Grep returns 1 if no match is found, so we use `fail_on_error=False` to suppress the error
-    if run_command(f"juju models | grep -q {model_name}", capture_output=True, fail_on_error=False):
+    if run_command(
+        f"juju models | grep -q {model_name}", capture_output=True, fail_on_error=False
+    ):
         print(f"Juju model '{model_name}' already exists.")
     else:
         print(f"Juju model '{model_name}' does not exist. Creating it...")
@@ -118,7 +139,7 @@ def ensure_model_exists(model_name, lxd_profile_path):
     )
     if controller_cloud in ["localhost", "lxd"]:
         print(
-            f"Current Juju controller is using LXD/localhost. Applying 'k8s.profile' to the model..."
+            "Current Juju controller is using LXD/localhost. Applying 'k8s.profile' to the model..."
         )
         run_command(f"lxc profile edit juju-{model_name} < {lxd_profile_path}")
     else:
@@ -126,10 +147,13 @@ def ensure_model_exists(model_name, lxd_profile_path):
 
 
 def main():
+    """Main entrypoint."""
     script_dir = Path(os.path.dirname(os.path.abspath(__file__)))
 
     parser = argparse.ArgumentParser(description="Terraform and Juju setup script.")
-    parser.add_argument("--terraform-version", default="latest/stable", help="Expected Terraform version.")
+    parser.add_argument(
+        "--terraform-version", default="latest/stable", help="Expected Terraform version."
+    )
     parser.add_argument(
         "--terraform-module-path",
         default=script_dir / "data",
@@ -166,17 +190,21 @@ def main():
     run_command("terraform init")
 
     print(
-        f"Running 'terraform plan' with manifest: {args.manifest_path} and model: {args.model_name}..."
+        f"Running 'terraform plan' with manifest: {args.manifest_path} "
+        f"and model: {args.model_name}..."
     )
     run_command(
-        f"terraform plan -var='manifest_path={args.manifest_path}' -var='model_name={args.model_name}'"
+        f"terraform plan -var='manifest_path={args.manifest_path}' "
+        f"-var='model_name={args.model_name}'"
     )
 
     print(
-        f"Running 'terraform apply' with manifest: {args.manifest_path} and model: {args.model_name}..."
+        f"Running 'terraform apply' with manifest: {args.manifest_path} "
+        f"and model: {args.model_name}..."
     )
     run_command(
-        f"terraform apply -var='manifest_path={args.manifest_path}' -var='model_name={args.model_name}' -auto-approve"
+        f"terraform apply -var='manifest_path={args.manifest_path}' "
+        f"-var='model_name={args.model_name}' -auto-approve"
     )
 
 
