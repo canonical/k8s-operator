4fa02186db4a3dc983a8da1f8b1ad298e614584a
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-02-12 12:30:39 +0100

    ignore constraint changes

--- charms/worker/k8s/terraform/main.tf
+++ charms/worker/k8s/terraform/main.tf
@@ -22,4 +22,11 @@ resource "juju_application" "k8s" {
   constraints = var.constraints
   units       = var.units
   resources   = var.resources
+
+  # Juju converts GB into MB internally for constraints.
+  # This let's terraform fail as expected state != actual state.
+  # This is a workaround to ignore the constraints change.
+  lifecycle {
+    ignore_changes = [constraints]
+  }
 }
