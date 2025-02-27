b03fe4c0a043b02a096d0da94dbee9ada5af2962
Adam Dyess <adam.dyess@canonical.com>
2025-02-27 12:26:35 -0600

    Patch up to match new ceph integration definition

--- tests/integration/data/main.tf
+++ tests/integration/data/main.tf
@@ -6,27 +6,48 @@ terraform {
   required_providers {
     juju = {
       source  = "juju/juju"
-      version = "~> 0.14.0"
+      version = ">= 0.14.0, < 1.0.0"
     }
   }
 }
 
-provider "juju" {}
-
 variable "manifest_yaml" {
   description = "Path to the manifest YAML file"
   type        = string
-  default     = "k8s-manifest.yaml"
 }
 
 variable "model" {
   description = "Name of the model to deploy to"
   type        = string
-  default     = "my-canonical-k8s"
+}
+
+variable "cloud" {
+  description = "Cloud to deploy to"
+  type        = string
+}
+
+variable "cloud_integration" {
+  description = "Selection of a cloud integration."
+  type        = string
+  default     = ""
+}
+
+variable "csi_integration" {
+  description = "Selection of a csi integration."
+  type        = list(string)
+  default     = []
 }
 
 module "k8s" {
   source        = "git::https://github.com/canonical/k8s-bundles//terraform?ref=main"
-  model         = var.model
+  model         = {
+    name = var.model
+    cloud = var.cloud
+    config = {
+      "test" = true
+    }
+  }
+  cloud_integration = var.cloud_integration
+  csi_integration = var.csi_integration
   manifest_yaml = var.manifest_yaml
 }
