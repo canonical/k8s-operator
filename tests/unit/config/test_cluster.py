c9ff714f2ece5c9fb91d245da9f478360f38d373
Adam Dyess <adam.dyess@canonical.com>
2025-06-27 13:52:42 -0500

    Move away from nested tox.ini

--- tests/unit/config/test_cluster.py
+++ tests/unit/config/test_cluster.py
@@ -16,22 +16,6 @@ from charm import K8sCharm
 from config.cluster import assemble_cluster_config
 
 
-@pytest.fixture(params=["worker", "control-plane"])
-def harness(request):
-    """Craft a ops test harness.
-
-    Args:
-        request: pytest request object
-    """
-    meta = Path(__file__).parent / "../../../charmcraft.yaml"
-    if request.param == "worker":
-        meta = Path(__file__).parent / "../../../../charmcraft.yaml"
-    harness = ops.testing.Harness(K8sCharm, meta=meta.read_text())
-    harness.begin()
-    harness.charm.is_worker = request.param == "worker"
-    yield harness
-    harness.cleanup()
-
 
 def test_configure_network_options(harness):
     """Test configuring the network options.
---
+++ tests/unit/conftest.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import ops.testing
+import pytest
+import charm
+
+
+@pytest.fixture(params=["worker", "control-plane"])
+def harness(request):
+    """Craft a ops test harness.
+
+    Args:
+        request: pytest request object
+    """
+
+    meta = Path(charm.__file__).parent / "../charmcraft.yaml"
+    if request.param == "worker":
+        meta = Path(charm.__file__).parent / "../../charmcraft.yaml"
+    harness = ops.testing.Harness(charm.K8sCharm, meta=meta.read_text())
+    harness.begin()
+    harness.charm.is_worker = request.param == "worker"
+    yield harness
+    harness.cleanup()
+
+
+
+@pytest.fixture(autouse=True)
+def mock_juju_public_address(monkeypatch):
+    """Mock _get_juju_public_address to return."""
+    monkeypatch.setattr(charm, "_get_juju_public_address", lambda: "127.0.0.1")
