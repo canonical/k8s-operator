9b56c8a99a17c2dd4d10f296898122ce15d54ef8
Smail Kourta <smail.kourta@canonical.com>
2025-07-25 12:35:59 +0000

    add rotation integration tests

--- tests/integration/test_charmed_etcd.py
+++ tests/integration/test_charmed_etcd.py
@@ -5,17 +5,72 @@
 
 """Integration tests."""
 
+import base64
 import json
+from typing import Literal
 
 import pytest
 from helpers import ready_nodes
-from juju import model, unit
+from juju import application, model, unit
 
 # This pytest mark configures the test environment to use the Canonical Kubernetes
 # bundle with etcd, for all the test within this module.
 pytestmark = [
     pytest.mark.bundle(file="test-bundle-charmed-etcd.yaml", apps_local=["k8s", "k8s-worker"])
 ]
+
+
+async def get_certificate_from_k8s(
+    kubernetes_cluster: model.Model, certificate: Literal["client", "ca"] = "client"
+):
+    """Get the certificate from a k8s unit."""
+    k8s_unit: unit.Unit = kubernetes_cluster.applications["k8s"].units[0]
+    event = await k8s_unit.run(f"sudo cat /etc/kubernetes/pki/etcd/{certificate}.crt")
+    result = await event.wait()
+    if result.status != "completed":
+        raise RuntimeError("Failed to get certificate from k8s")
+    cert = result.results["stdout"].strip()
+    assert cert, "Certificate is empty"
+    return cert
+
+
+async def get_client_cas_etcd(kubernetes_cluster: model.Model):
+    """Get the client CA from the etcd unit."""
+    etcd: unit.Unit = kubernetes_cluster.applications["charmed-etcd"].units[0]
+    event = await etcd.run("sudo cat /var/snap/charmed-etcd/current/tls/client_ca.pem")
+    result = await event.wait()
+    if result.status != "completed":
+        raise RuntimeError("Failed to get certificate from etcd")
+    etcd_client_cas = result.results["stdout"].strip()
+    assert etcd_client_cas, "etcd client CA is empty"
+    return etcd_client_cas
+
+
+async def assert_cluster_ready(kubernetes_cluster: model.Model):
+    """Assert that the k8s cluster is ready."""
+    k8s: unit.Unit = kubernetes_cluster.applications["k8s"].units[0]
+    event = await k8s.run("k8s status --output-format json")
+    result = await event.wait()
+    status = json.loads(result.results["stdout"])
+    assert status["ready"], "Cluster isn't ready"
+
+
+async def get_etcd_tls_ca(kubernetes_cluster: model.Model):
+    """Get the etcd TLS CA from the secrets."""
+    secrets = await kubernetes_cluster.list_secrets(show_secrets=True)
+    assert secrets, "No secrets found in the model"
+    etcd_tls_ca_secret = next(
+        (
+            s
+            for s in secrets
+            if s.owner_tag == "application-charmed-etcd" and "tls-ca" in s.value.data
+        ),
+        None,
+    )
+    assert etcd_tls_ca_secret, "etcd TLS CA secret not found"
+    tls_ca = base64.b64decode(etcd_tls_ca_secret.value.data["tls-ca"]).decode("utf-8")
+    assert tls_ca, "etcd TLS CA is empty"
+    return tls_ca
 
 
 @pytest.mark.abort_on_fail
@@ -49,6 +104,7 @@ async def test_update_etcd_cluster(kubernetes_cluster: model.Model):
     count = 3 - len(etcd.units)
     if count > 0:
         await etcd.add_unit(count=count)
+
     await kubernetes_cluster.wait_for_idle(status="active", timeout=20 * 60)
 
     expected_servers = []
@@ -62,3 +118,46 @@ async def test_update_etcd_cluster(kubernetes_cluster: model.Model):
     assert status["ready"], "Cluster isn't ready"
     assert status["datastore"]["type"] == "external", "Not bootstrapped against etcd"
     assert set(status["datastore"]["servers"]) == set(expected_servers)
+
+
+@pytest.mark.abort_on_fail
+async def test_certificate_rotation_k8s(kubernetes_cluster: model.Model):
+    """Test apiserver certificate rotation."""
+    old_cert_k8s = await get_certificate_from_k8s(kubernetes_cluster)
+    old_client_cas_etcd = await get_client_cas_etcd(kubernetes_cluster)
+    assert old_cert_k8s in old_client_cas_etcd, "Old cert not in etcd client CA"
+
+    ssc_k8s: application.Application = kubernetes_cluster.applications["ssc-k8s"]
+    await ssc_k8s.set_config({"ca-common-name": "NEW_CN_CA"})
+
+    await kubernetes_cluster.wait_for_idle(status="active", timeout=20 * 60)
+
+    new_cert_k8s = await get_certificate_from_k8s(kubernetes_cluster)
+    new_client_cas_etcd = await get_client_cas_etcd(kubernetes_cluster)
+    assert new_cert_k8s != old_cert_k8s, "Certificate did not rotate"
+    assert new_cert_k8s in new_client_cas_etcd, "New cert not in etcd client CA"
+    assert old_cert_k8s not in new_client_cas_etcd, "Old cert still in etcd client CA"
+    await assert_cluster_ready(kubernetes_cluster)
+
+
+@pytest.mark.abort_on_fail
+async def test_certificate_rotation_etcd(kubernetes_cluster: model.Model):
+    """Test etcd TLS CA rotation."""
+    current_etcd_tls_ca = await get_etcd_tls_ca(kubernetes_cluster)
+    assert current_etcd_tls_ca, "Current etcd TLS CA is empty"
+    current_k8s_client_ca = await get_certificate_from_k8s(kubernetes_cluster, certificate="ca")
+    assert current_k8s_client_ca, "Current k8s client CA is empty"
+    assert current_etcd_tls_ca == current_k8s_client_ca, "etcd TLS CA does not match k8s client CA"
+
+    ssc_etcd: application.Application = kubernetes_cluster.applications["ssc-charmed-etcd"]
+    await ssc_etcd.set_config({"ca-common-name": "NEW_ETCD_CN_CA"})
+
+    await kubernetes_cluster.wait_for_idle(status="active", timeout=20 * 60)
+
+    new_etcd_tls_ca = await get_etcd_tls_ca(kubernetes_cluster)
+    assert new_etcd_tls_ca, "New etcd TLS CA is empty"
+    new_k8s_client_ca = await get_certificate_from_k8s(kubernetes_cluster, certificate="ca")
+    assert new_k8s_client_ca, "New k8s client CA is empty"
+    assert new_etcd_tls_ca == new_k8s_client_ca, "New etcd TLS CA does not match new k8s client CA"
+
+    await assert_cluster_ready(kubernetes_cluster)
