7df9ce69a1b7ffaa77d2d1b68139780dde2ae050
Mateo Florido <mateoflosan@gmail.com>
2025-12-09 16:51:18 -0500

    Revert "release: Prepare for `1.34` (#789)"
    This reverts commit 0476898e71d21d90c5846c94cce7446da50a1a01.

--- charms/worker/k8s/src/literals.py
+++ charms/worker/k8s/src/literals.py
@@ -183,10 +183,10 @@ DEPENDENCIES = {
     },
     # NOTE: Update the dependencies for the k8s-service before releasing.
     "k8s_service": {
-        "dependencies": {"k8s-worker": "^1.33, < 1.35"},
+        "dependencies": {"k8s-worker": "^1.32, < 1.34"},
         "name": "k8s",
-        "upgrade_supported": "^1.33, < 1.35",
-        "version": "1.34.0",
+        "upgrade_supported": "^1.32, < 1.34",
+        "version": "1.33.0",
     },
 }
 
