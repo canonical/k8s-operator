00e46d2b8559a21a08a5e7a706a69f0903e3b4ac
Adam Dyess <adam.dyess@canonical.com>
2026-03-30 09:17:03 -0500

    Update TF release

--- tests/integration/terraform/main.tf
+++ tests/integration/terraform/main.tf
@@ -39,7 +39,7 @@ variable "csi_integration" {
 }
 
 module "k8s" {
-  source        = "git::https://github.com/canonical/k8s-bundles//terraform?ref=main"
+  source        = "git::https://github.com/canonical/k8s-bundles//terraform?ref=release-1.35"
   model         = {
     name = var.model
     cloud = var.cloud
